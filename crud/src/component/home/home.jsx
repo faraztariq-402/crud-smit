e7157599d0b55d836f22cab8df9fd1912a8f0a8c
faraztariq-402 <faraztariq12123@gmail.com>
2023-09-21 06:21:26 -0700

    code

--- crud/src/component/home/home.jsx
+++ crud/src/component/home/home.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import './home.css'
-const baseUrl = "http://localhost:5001";
+// const baseUrl = "http://localhost:5001";
 
 const Post = () => {
     const userPostTitle = useRef(null);
@@ -9,31 +9,22 @@ const Post = () => {
     const [allPosts, setAllPosts] = useState([])
     const searchInputRef = useRef(null)
 
-    // const searchHandler = async (e) => {
-    //     e.preventDefault();
-    //     try {
-    //         const response = await axios.get(`/api/v1/search?q=${searchInputRef.current.value}`);
-    //         // console.log(response.data);
-
-    //         setAllPosts([...response.data]);
-    //     } catch (error) {
-    //         console.log(error.data);
-    //     }
-    //     e.target.reset()
-    // }
     const submitPostHandler = async (e) => {
         e.preventDefault();
 
         // Get the input values inside the submitPostHandler function
         const titleValue = userPostTitle.current.value;
         const textValue = userPostText.current.value;
-
+if(!titleValue && !textValue){
+    alert("Please enter your values")
+    return
+}
 
         console.log(titleValue);
         console.log(textValue);
 
         try {
-            const response = await axios.post(`${baseUrl}/api/v1/post`, {
+            const response = await axios.post(`/api/v1/post`, {
                 title: titleValue,
                 text: textValue,
             });
@@ -47,7 +38,7 @@ const Post = () => {
     };
     const getAllPost = async () => {
         try {
-            const response = await axios.get(`${baseUrl}/api/v1/posts`);
+            const response = await axios.get(`/api/v1/posts`);
             console.log(response.data);
             // console.log(response.data.text);
             setAllPosts([...response.data])
@@ -66,6 +57,7 @@ const Post = () => {
     const searchHandler = async (e) => {
         e.preventDefault();
         try {
+            console.log("searchInputRef value:", searchInputRef.current.value);
             const response = await axios.get(`/api/v1/search?q=${searchInputRef.current.value}`);
 
             console.log(response.data);
@@ -79,16 +71,16 @@ const Post = () => {
 
     return (
         <>
-            <form action="" onSubmit={submitPostHandler}>
-                <label htmlFor="postTitle">Post Title:</label>
-                <input type="text" placeholder="Enter a post title..." ref={userPostTitle} />
-                <label htmlFor="postText">Post Text:</label>
-                <input type="text" placeholder="Enter a post text..." ref={userPostText} />
-                <button type="submit">Publish Post..</button>
-            </form>
-            <form action="" style={{ textAlign: "right" }} onSubmit={searchHandler}>
+            <form action="" className="search" style={{ textAlign: "center" }} onSubmit={searchHandler}>
                 <input type="search" placeholder="Search..." ref={searchInputRef} />
                 <button type="submit" hidden></button>
+            </form>
+            <form action="" className="submit" onSubmit={submitPostHandler}>
+                <label htmlFor="postTitle">Post Title:</label><br />
+                <input type="text" placeholder="Enter a post title..." ref={userPostTitle} /><br />
+                <label htmlFor="postText">Post Text:</label><br />
+                <input type="text" placeholder="Enter a post text..." ref={userPostText} />
+                <button type="submit">Publish Post..</button>
             </form>
             <div className="allPosts">
 
